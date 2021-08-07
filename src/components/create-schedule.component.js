da77d564f01f91c147f17cdfe1d547986af8aee9
mahroo12 <mahroo.rahman@gmail.com>
2021-08-07 18:11:43 -0400

    removed inline styling

--- src/components/create-schedule.component.js
+++ src/components/create-schedule.component.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Link} from "react-router-dom";
 import Modal from "./modal.component";
+import "./createSchedule.css";
 
 
 export default class CreateSchedule extends Component {
@@ -16,121 +17,142 @@ export default class CreateSchedule extends Component {
     {
         
         return(
-            <div style={{paddingBottom: '50px'}}>
-                <div style={{
-            
-                
-                backgroundColor: '#FFFFFF', 
-                width: '140vh',
-                height: '470px',
-                position: 'relative',
-                zIndex: '1',
-                boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
-                marginTop: '40px',
-                marginLeft: '220px',
-                marginBottom: '100px',
-                paddingLeft: '15px',
-                borderRadius: '10px',
-            
-            
-                            }}>
-                    <h3 style={{paddingTop: '50px', paddingLeft:'5px', textAlign: 'center'}}>Computer List</h3>
-                    <div style={{display: 'flex', justifyContent: 'center', paddingTop: '30px'}}>
-                    <div className="compList">
-                        <table style={{bordercollapse: 'collapse'}}>
-                            <thead style= {{backgroundColor: '#EEE', padding: '10px'}}>
-                                <tr>
-                                    <th style={{borderRadius: '5px 0 0 5px', paddingLeft: '50px', textAlign: 'center'}}>ID </th>
-                                    <th style={{paddingLeft: '50px', textAlign: 'center'}}>Operating System </th>
-                                    
-                                    <th style={{borderRadius:'0 5px 5px 0',paddingLeft:'50px', width: '300px', textAlign: 'center'}}>Schedule </th>
-                                </tr>
-                            </thead>   
-                            <tbody >
-                                <tr >
-                                    <td style={{paddingLeft:'50px', textAlign: 'center'}}>ID7676</td>
-                                    <td style={{paddingLeft:'50px', textAlign: 'center'}}>linux</td>
-                                    
-                                    <td style={{paddingLeft:'50px',align:'center', zIndex: '1'}}>
-                                        <form >
-                                            <div style={{display: 'flex', justifyContent: 'center'}}>
-                                                <div>
-                                                    <input style={{margin: '5px'}} type= "submit" value="View" className="btn btn-primary" onClick={(e) => this.setState({ openModal: true}) & e.preventDefault()}/>
-                                                </div>
-                                                <div style= {{paddingTop: '5px'}}>
-                                                    <Link to="/calendar" className="btn btn-primary">Book</Link>
+            <div className= "top-div">
+                <div className= "white-div" >
+                    
+                    <h3 className= "computer-list-label">Computer List</h3>
+                    
+                    <div className= "div-to-centre-table" >
+                        
+                        <div className="compList">
+                            <table className= "table-cl">
                                 
-                                            
+                                <thead className="table-head">
+                                 <tr>
+                                    <th className="head-id" >ID </th>
+                                    <th className="head-os">Operating System </th>
+                                    <th className="head-schedule">Schedule </th>
+                                 </tr>
+                                </thead>  
+
+                                <tbody >
+                                 <tr >
+                                    <td className="row-content">ID7676</td>
+                                    <td className="row-content">linux</td>
+                                    <td className="row-content-exp">
+                                        <form >
+                                            <div className="form-div">
+                                                
+                                                <div>
+                                                    <input style={{margin: '5px'}} 
+                                                        type= "submit" value="View" 
+                                                        className="btn btn-primary" 
+                                                        onClick={(e) => this.setState({ openModal: true}) & e.preventDefault()}
+                                                    />
+                                                </div>
+
+                                                <div className ="book-button-div">
+                                                    <Link to="/calendar" 
+                                                        className="btn btn-primary">Book
+                                                    </Link>
                                                 </div>
                                             </div>
                                         </form>
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td style={{paddingLeft:'50px', textAlign: 'center'}}>ID7676</td>
-                                    <td style={{paddingLeft:'50px', textAlign: 'center'}}>linux</td>
-                                    
-                                    <td style={{paddingLeft:'50px',align:'center'}}>
+                                    <td className="row-content">ID7676</td>
+                                    <td className="row-content">linux</td>
+                                    <td className="row-content2">
                                         <form >
-                                            <div style={{display: 'flex', justifyContent: 'center'}}>
-                                            <div>
-                                            <input style={{margin: '5px'}} type= "submit" value="View" className="btn btn-primary" />
-                                            </div>
-                                            <div style= {{paddingTop: '5px'}}>
-                                            <Link to="/calendar" className="btn btn-primary">Book</Link>
-                                            </div >
+                                            <div className="form-div">
+                                                <div>
+                                                    <input style={{margin: '5px'}} 
+                                                        type= "submit" 
+                                                        value="View" 
+                                                        className="btn btn-primary" 
+                                                    />
+                                                </div>
+                                                
+                                                <div className ="book-button-div">
+                                                    <Link to="/calendar" 
+                                                        className="btn btn-primary">Book
+                                                    </Link>
+                                                </div >
+                                            
                                             </div>
                                         </form>
                                     </td>
                                 </tr>
 
                                 <tr >
-                                    <td style={{paddingLeft:'50px', textAlign: 'center'}}>ID7675</td>
-                                    <td style={{paddingLeft:'50px', textAlign: 'center'}}>linux</td>
+                                    <td className="row-content">ID7675</td>
+                                    <td className="row-content">linux</td>
                                     
-                                    <td style={{paddingLeft:'50px',align:'center'}}>
-                                    <form >
-                                            <div style={{display: 'flex', justifyContent: 'center'}}>
-                                            <div>
-                                            <input style={{margin: '5px'}} type= "submit" value="View" className="btn btn-primary" />
-                                            </div>
-                                            <div style= {{paddingTop: '5px'}}>
-                                            <Link to="/calendar" className="btn btn-primary">Book</Link>
-                                            </div >
-                                            </div>
-                                        </form>
-                                    </td>
-                                </tr>
-                                <tr >
-                                    <td style={{paddingLeft:'50px', textAlign: 'center'}}>ID7696</td>
-                                    <td style={{paddingLeft:'50px', textAlign: 'center'}}>linux</td>
-                                    
-                                    <td style={{paddingLeft:'50px',align:'center'}}>
-                                    <form >
-                                            <div style={{display: 'flex', justifyContent: 'center'}}>
-                                            <div>
-                                            <input style={{margin: '5px'}} type= "submit" value="View" className="btn btn-primary" />
-                                            </div>
-                                            <div style= {{paddingTop: '5px'}}>
-                                            <Link to="/calendar" className="btn btn-primary">Book</Link>
-                                            </div >
-                                            </div>
-                                        </form>
-                                    </td>
-                                </tr>
-                                <tr >
-                                    <td style={{paddingLeft:'50px', textAlign: 'center'}}>ID0676</td>
-                                    <td style={{paddingLeft:'50px', textAlign: 'center'}}>linux</td>
-                                    
-                                    <td style={{paddingLeft:'50px',align:'center'}}>
+                                    <td className="row-content2">
                                         <form >
-                                            <div style={{display: 'flex', justifyContent: 'center'}}>
-                                            <div>
-                                            <input style={{margin: '5px'}} type= "submit" value="View" className="btn btn-primary" />
+                                            <div className="form-div">
+                                                <div>
+                                                    <input style={{margin: '5px'}} 
+                                                            type= "submit" 
+                                                            value="View" 
+                                                            className="btn btn-primary" 
+                                                    />
+                                                </div>
+                                                
+                                                <div className ="book-button-div">
+                                                    <Link to="/calendar" 
+                                                        className="btn btn-primary">Book
+                                                    </Link>
+                                                </div >
                                             </div>
-                                            <div style= {{paddingTop: '5px'}}>
-                                            <Link to="/calendar" className="btn btn-primary">Book</Link>
-                                            </div >
+                                        </form>
+                                    </td>
+                                </tr>
+
+                                <tr >
+                                    <td className="row-content">ID7696</td>
+                                    <td className="row-content">linux</td>
+                                    <td className="row-content2">
+                                        <form >
+                                            <div className="form-div">
+                                                <div>
+                                                    <input style={{margin: '5px'}} 
+                                                           type= "submit" 
+                                                           value="View" 
+                                                           className="btn btn-primary" 
+                                                    />
+                                                </div>
+                                            
+                                                <div className ="book-button-div">
+                                                    <Link to="/calendar" 
+                                                        className="btn btn-primary">Book
+                                                    </Link>
+                                                </div >
+                                            </div>
+                                        </form>
+                                    </td>
+                                </tr>
+
+                                <tr >
+                                    <td className="row-content">ID0676</td>
+                                    <td className="row-content">linux</td>
+                                    <td className="row-content2">
+                                        <form >
+                                            <div className="form-div">
+                                                <div>
+                                                    <input style={{margin: '5px'}} 
+                                                            type= "submit" value="View" 
+                                                            className="btn btn-primary" 
+                                                    />
+                                                </div>
+                                            
+                                                <div className ="book-button-div">
+                                                    <Link to="/calendar" 
+                                                        className="btn btn-primary">Book
+                                                    </Link>
+                                                </div >
+                                            
                                             </div>
                                         </form>
                                     </td>
