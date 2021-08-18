a71ed8bddfd3091c01cc7f2c8f1fd0575f2f3fbb
mahroo12 <mahroo.rahman@gmail.com>
2021-08-18 11:08:43 -0400

    added footer

--- src/components/home-page.component.js
+++ src/components/home-page.component.js
@@ -6,16 +6,38 @@ export default class Home extends Component {
    render(){
         return(
         
-            
+            <div>
                
-                   <div className="home">
+            <div className="home">
                <div>
-               <Link to="/schedule" className="btn btn-primary" style={{marginLeft: '80px', marginTop: '220px', paddingLeft: '20px', paddingRight: '20px', paddingTop: '13px', paddingBottom: '13px'}}> Computers</Link>
+                <Link to="/schedule" className="btn btn-primary" style={{marginLeft: '80px', marginTop: '220px', paddingLeft: '20px', paddingRight: '20px', paddingTop: '13px', paddingBottom: '13px'}}> Computers</Link>
                </div>
                <div>
-               <Link to="#" className="btn btn-primary" style={{marginLeft: '80px', marginTop: '20px'}}>others</Link>
+                <Link to="#" className="btn btn-primary" style={{marginLeft: '80px', marginTop: '20px'}}>others</Link>
                </div>
-               </div>
+            </div>
+
+            <footer style={{backgroundColor: '#566AB1', display: 'flex', justifyContent: 'space-between'}}>
+                
+                
+                        <Link to="/" className="nav-link" style={{color: 'white'}}>vpcomm@mcgilleus.ca</Link>
+                
+                
+                <div style={{display: 'flex', justifyContent: 'flex-end', paddingRight: '20px'}}>
+                <ul className="navbar-nav mr-auto" style={{paddingRight: '20px'}}>
+                    <li >
+                        <Link to="/" className="nav-link" style={{color: 'white'}}>fb</Link>
+                    </li>
+                </ul>
+                
+                <ul className="navbar-nav mr-auto">
+                    <li >
+                        <Link to="/" className="nav-link" style={{color: 'white'}}>insta</Link>
+                    </li>
+                </ul>
+                </div>
+            </footer>
+            </div>
             
         
     )}
