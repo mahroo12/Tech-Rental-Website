10e7819a54e2d009c0fabd1959de7ad9098fdbb0
mahroo12 <mahroo.rahman@gmail.com>
2021-08-18 11:18:51 -0400

    buttons in home page styled

--- src/components/home-page.component.js
+++ src/components/home-page.component.js
@@ -10,10 +10,10 @@ export default class Home extends Component {
                
             <div className="home">
                <div>
-                <Link to="/schedule" className="btn btn-primary" style={{marginLeft: '80px', marginTop: '220px', paddingLeft: '20px', paddingRight: '20px', paddingTop: '13px', paddingBottom: '13px'}}> Computers</Link>
+                <Link to="/schedule" className="btn btn-primary" style={{border: 'none', backgroundColor: '#566AB1', marginLeft: '80px', marginTop: '220px', paddingLeft: '20px', paddingRight: '20px', paddingTop: '13px', paddingBottom: '13px'}}> Computers</Link>
                </div>
                <div>
-                <Link to="#" className="btn btn-primary" style={{marginLeft: '80px', marginTop: '20px'}}>others</Link>
+                <Link to="#" className="btn btn-primary" style={{border: 'none', color: '#566AB1', backgroundColor: '#FFFFFF40',marginLeft: '80px', marginTop: '20px'}}>Others</Link>
                </div>
             </div>
 
