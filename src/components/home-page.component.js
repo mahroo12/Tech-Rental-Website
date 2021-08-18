9670c1842cfc123caf1540923ccf629e92fa4d95
mahroo12 <mahroo.rahman@gmail.com>
2021-08-17 20:31:22 -0400

    added home page component

---
+++ src/components/home-page.component.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+
+export default class Home extends Component {
+   render(){
+        return(
+        <div>
+            <div className= "home">
+                <h1>hi </h1>
+            </div>
+        </div>
+    )}
+}
